37b3b5e1d7df113ec014a01ed16e57a06eb677f0
Swarm Coders <seajure@googlegroups.com>
2012-08-03 06:52:14 -0700

    final status after night if seajure

--- src/poker_kata/core.clj
+++ src/poker_kata/core.clj
@@ -1,6 +1,51 @@
 (ns poker-kata.core)
 
-(defn -main
-  "I don't do a whole lot."
-  [& args]
-  (println "Hello, World!"))
+(def face-values {\2 2 \3 3 \4 4 \5 5 \6 6 \7 7 \8 8 \9 9 \T 10 \J 11 \Q 12 \K 13 \A 14})
+(def suits {\D :diamonds \C :clubs \H :hearts \S :spades})
+(def suit-value {:diamonds 0 :clubs 1 :hearts 2 :spades 3})
+
+(defn parse-card [[value suit]]
+  (vector (face-values value)
+          (suits suit)))
+
+(defn compare-cards [[v1 s1] [v2 s2]]
+  (if (= v1 v2)
+    (compare (suit-value s1) (suit-value s2))
+    (compare v1 v2)))
+
+(defn sort-cards [cards]
+  (sort compare-cards cards))
+
+(defn parse-hand [s]
+  (sort-cards (mapv parse-card (.split s " "))))
+
+(defn highest [cards]
+  (last (sort-cards cards)))
+
+(defn high-card? [cards]
+  :high-card)
+
+(defn straight? [[[lowest-val _] & rest-of-cards :as all-the-cards]]
+  (when (= (map first all-the-cards)
+           (take (count all-the-cards) (iterate inc lowest-val)))
+    :straight))
+
+(def foo [[:straight straight?] [:high-card high-card?]])
+
+(defn categorize-hand [hand]
+  (some #(% (parse-hand hand)) (map second foo)))
+
+(defmulti tie-break (fn [h _] (categorize-hand h)))
+
+(defmethod tie-break :high-card [h1 h2]
+  (drop-while zero? (map compare-cards (reverse h1) (reverse h2))))
+
+(defmethod tie-break :straight [[f1 & _] [f2 & _]]
+  (compare-cards f1 f2))
+
+(defn compare-hands [hand1 hand2]
+  (let [category1-value (.indexOf (map first foo) (categorize-hand hand1))
+        category2-value (.indexOf (map first foo) (categorize-hand hand2))]
+    (if (= category1-value category2-value)
+      (tie-break (parse-hand hand1) (parse-hand hand2))
+      (compare category1-value category2-value))))
--- test/poker_kata/core_test.clj
+++ test/poker_kata/core_test.clj
@@ -2,6 +2,34 @@
   (:use clojure.test
         poker-kata.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest parsing-card
+  (is (= [2 :hearts] (parse-card "2H")))
+  (is (= [10 :clubs] (parse-card "TC")))
+  (is (= [14 :spades] (parse-card "AS"))))
+
+(deftest parsing-hands
+  (is (= [[2 :hearts] [3 :diamonds] [5 :spades] [9 :clubs] [13 :diamonds]]
+         (parse-hand "2H 3D 5S 9C KD"))))
+
+(deftest sorting-cards
+  (is (= [[2 :diamonds] [4 :diamonds]]
+         (sort-cards [[4 :diamonds] [2 :diamonds]])))
+  (is (= [[5 :hearts] [5 :spades]]
+         (sort-cards [[5 :spades] [5 :hearts]]))))
+
+(deftest finding-the-highest-value
+  (is (= [10 :spades]
+         (highest [[10 :spades] [10 :diamonds]])))
+  (is (= [10 :hearts]
+         (highest [[5 :spades] [10 :hearts]]))))
+
+
+(deftest categorize-hands
+  (is (= :high-card
+         (categorize-hand "2H 3S 4S 5C 7H")))
+  (is (= :straight
+         (categorize-hand "2H 3S 4S 5C 6H"))))
+
+(deftest comparing-hands
+  (is (= 1
+         (compare-hands "2H 3S 4S 5C 6H" "3H 4S 5S 6C 7H"))))
