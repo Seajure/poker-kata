49686dac63bb082c6669634ed442a7f45f4e8ecf
Swarm Coders <seajure@googlegroups.com>
2012-08-02 18:28:08 -0700

    initial commit
    includes description

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject poker-kata "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :plugins [[lein-swank "1.4.4"]])
---
+++ test/poker_kata/core_test.clj
@@ -0,0 +1,7 @@
+(ns poker-kata.core-test
+  (:use clojure.test
+        poker-kata.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
